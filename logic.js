4123879aadc6b4439585c01900e3b032398747cc
Chris <chris.kieran@gmail.com>
2019-01-06 21:18:35 -0500

    2019 Site/Portfolio
    - more information for freelance clients
    - better looking UI

---
+++ logic.js
@@ -0,0 +1,8 @@
+function toggleSiteMap() {
+    var x = document.getElementById("siteMap");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
